8bafa7e6e46bcf330204604825f71ecbbad8558e
ghorsington <neigh@coder.horse>
2020-01-02 21:43:16 +0200

    Add test for IL resolving of <PrivateImplementationDetails>

---
+++ HarmonyTests/Extras/Assets/PrivateImplDetailsInvokerClasses.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace HarmonyLibTests.Assets
+{
+    public class PrivateImplDetailsInvokerObjectPatch
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void Prefix(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    public class PrivateImplDetailsInvokerObject
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Something(string val)
+        {
+
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void Test(string value)
+        {
+            switch (value)
+            {
+                case "A":
+                    Something("A branch");
+                    break;
+
+                case "B":
+                    Something("B branch");
+                    break;
+
+                case "C":
+                    Something("C branch");
+                    break;
+
+                case "D":
+                    Something("D branch");
+                    break;
+
+                case "E":
+                    Something("E branch");
+                    break;
+
+                case "F":
+                    Something("F branch");
+                    break;
+
+                case "G":
+                    Something("G branch");
+                    break;
+
+                case "H":
+                    Something("H branch");
+                    break;
+
+                case "I":
+                    Something("I branch");
+                    break;
+
+                case "J":
+                    Something("J branch");
+                    break;
+
+                case "K":
+                    Something("K branch");
+                    break;
+
+                case "L":
+                    Something("L branch");
+                    break;
+
+                case "M":
+                    Something("M branch");
+                    break;
+
+                default:
+                    Something("default branch");
+                    break;
+            }
+
+            Something("Complete");
+        }
+    }
+}
---
+++ HarmonyTests/Extras/TestPrivateImplDetailsInvoker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HarmonyLib;
+using HarmonyLibTests.Assets;
+using NUnit.Framework;
+
+namespace HarmonyLibTests
+{
+    [TestFixture]
+    public class TestPrivateImplDetailsInvoker
+    {
+        [Test]
+        public void TestPrivateImplDetailsInvokerGeneral()
+        {
+            var originalClass = typeof(PrivateImplDetailsInvokerObject);
+            Assert.IsNotNull(originalClass);
+            var originalMethod = originalClass.GetMethod(nameof(PrivateImplDetailsInvokerObject.Test));
+            Assert.IsNotNull(originalMethod);
+
+            var patchClass = typeof(PrivateImplDetailsInvokerObjectPatch);
+            var prefix = patchClass.GetMethod(nameof(PrivateImplDetailsInvokerObjectPatch.Prefix));
+            Assert.IsNotNull(prefix);
+
+            var instance = new Harmony("test");
+            Assert.IsNotNull(instance);
+
+            var patcher = new PatchProcessor(instance, originalMethod);
+            Assert.IsNotNull(patcher);
+            patcher.AddPrefix(prefix);
+
+            Environment.SetEnvironmentVariable("MONOMOD_DMD_TYPE", "mb");
+            patcher.Patch();
+
+            PrivateImplDetailsInvokerObject.Test("test");
+        }
+    }
+}
