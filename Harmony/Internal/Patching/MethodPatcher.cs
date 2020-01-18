f309995cf46cce72410fa3428438f7a390d52c43
ghorsington <neigh@coder.horse>
2020-01-18 17:47:59 +0200

    Add initial generic method patcher

---
+++ Harmony/Internal/Patching/MethodPatcher.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace HarmonyLib.Internal.Patching
+{
+    internal abstract class MethodPatcher
+    {
+        public MethodBase Original { get; }
+
+        protected MethodPatcher(MethodBase original)
+        {
+            Original = original;
+        }
+
+        public abstract void Apply();
+
+        public static MethodPatcher Create(MethodInfo original)
+        {
+            if(original.GetMethodBody() == null)
+                return new NativeMethodPatcher(original);
+            return new ILMethodPatcher(original);
+        }
+    }
+
+    internal class ILMethodPatcher : MethodPatcher
+    {
+        public ILMethodPatcher(MethodBase original) : base(original)
+        {
+
+        }
+
+        public override void Apply()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    internal class NativeMethodPatcher : MethodPatcher
+    {
+        public NativeMethodPatcher(MethodBase original) : base(original)
+        {
+
+        }
+
+        public override void Apply()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
