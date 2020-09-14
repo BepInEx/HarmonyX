e2eab6b292430df3262a4832ffa8b1f2850c5ec6
ghorsington <neigh@coder.horse>
2020-09-14 20:41:56 +0300

    Port EmitDelegate helper

--- Harmony/Public/Transpilers.cs
+++ Harmony/Public/Transpilers.cs
@@ -3,20 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using MonoMod.Utils;
 
 namespace HarmonyLib
 {
 	/// <summary>A collection of commonly used transpilers</summary>
-	/// 
+	///
 	public static class Transpilers
 	{
+		// ReSharper disable once CollectionNeverQueried.Local => used by generated DMDs
+		private static readonly Dictionary<int, Delegate> DelegateCache = new Dictionary<int, Delegate>();
+		private static int delegateCounter;
+
+		/// <summary>Returns an instruction to call the specified delegate</summary>
+		/// <typeparam name="T">The delegate type to emit</typeparam>
+		/// <param name="action">The delegate to emit</param>
+		/// <returns>The instruction to call the specified action</returns>
+		///
+		public static CodeInstruction EmitDelegate<T>(T action) where T : Delegate
+		{
+			if (action.Method.IsStatic && action.Target == null) return new CodeInstruction(OpCodes.Call, action.Method);
+
+			var paramTypes = action.Method.GetParameters().Select(x => x.ParameterType).ToArray();
+
+			var dynamicMethod = new DynamicMethodDefinition(action.Method.Name,
+				action.Method.ReturnType,
+				paramTypes);
+
+			var il = dynamicMethod.GetILGenerator();
+
+			var targetType = action.Target.GetType();
+
+			var preserveContext = action.Target != null && targetType.GetFields().Any(x => !x.IsStatic);
+
+			if (preserveContext)
+			{
+				var currentDelegateCounter = delegateCounter++;
+
+				DelegateCache[currentDelegateCounter] = action;
+
+				var cacheField = AccessTools.Field(typeof(Transpilers), nameof(DelegateCache));
+
+				var getMethod = AccessTools.Method(typeof(Dictionary<int, Delegate>), "get_Item");
+
+				il.Emit(OpCodes.Ldsfld, cacheField);
+				il.Emit(OpCodes.Ldc_I4, currentDelegateCounter);
+				il.Emit(OpCodes.Callvirt, getMethod);
+			}
+			else
+			{
+				if (action.Target == null)
+					il.Emit(OpCodes.Ldnull);
+				else
+					il.Emit(OpCodes.Newobj,
+						AccessTools.FirstConstructor(targetType, x => x.GetParameters().Length == 0 && !x.IsStatic));
+
+				il.Emit(OpCodes.Ldftn, action.Method);
+				il.Emit(OpCodes.Newobj, AccessTools.Constructor(typeof(T), new[] {typeof(object), typeof(IntPtr)}));
+			}
+
+			for (var i = 0; i < paramTypes.Length; i++)
+				il.Emit(OpCodes.Ldarg_S, (short)i);
+
+			il.Emit(OpCodes.Callvirt, AccessTools.Method(typeof(T), "Invoke"));
+			il.Emit(OpCodes.Ret);
+
+			return new CodeInstruction(OpCodes.Call, dynamicMethod.Generate());
+		}
+
 		/// <summary>A transpiler that replaces all occurrences of a given method with another one using the same signature</summary>
 		/// <param name="instructions">The enumeration of <see cref="CodeInstruction"/> to act on</param>
 		/// <param name="from">Method or constructor to search for</param>
 		/// <param name="to">Method or constructor to replace with</param>
 		/// <returns>Modified enumeration of <see cref="CodeInstruction"/></returns>
 		///
-		public static IEnumerable<CodeInstruction> MethodReplacer(this IEnumerable<CodeInstruction> instructions, MethodBase from, MethodBase to)
+		public static IEnumerable<CodeInstruction> MethodReplacer(this IEnumerable<CodeInstruction> instructions,
+			MethodBase from, MethodBase to)
 		{
 			if (from is null)
 				throw new ArgumentException("Unexpected null argument", nameof(from));
@@ -31,6 +93,7 @@ namespace HarmonyLib
 					instruction.opcode = to.IsConstructor ? OpCodes.Newobj : OpCodes.Call;
 					instruction.operand = to;
 				}
+
 				yield return instruction;
 			}
 		}
@@ -41,7 +104,8 @@ namespace HarmonyLib
 		/// <param name="action">An action to apply to matching instructions</param>
 		/// <returns>Modified enumeration of <see cref="CodeInstruction"/></returns>
 		///
-		public static IEnumerable<CodeInstruction> Manipulator(this IEnumerable<CodeInstruction> instructions, Func<CodeInstruction, bool> predicate, Action<CodeInstruction> action)
+		public static IEnumerable<CodeInstruction> Manipulator(this IEnumerable<CodeInstruction> instructions,
+			Func<CodeInstruction, bool> predicate, Action<CodeInstruction> action)
 		{
 			if (predicate is null)
 				throw new ArgumentNullException(nameof(predicate));
@@ -61,7 +125,8 @@ namespace HarmonyLib
 		/// <param name="text">The log text</param>
 		/// <returns>Modified enumeration of <see cref="CodeInstruction"/></returns>
 		///
-		public static IEnumerable<CodeInstruction> DebugLogger(this IEnumerable<CodeInstruction> instructions, string text)
+		public static IEnumerable<CodeInstruction> DebugLogger(this IEnumerable<CodeInstruction> instructions,
+			string text)
 		{
 			yield return new CodeInstruction(OpCodes.Ldstr, text);
 			yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(FileLog), nameof(FileLog.Log)));
