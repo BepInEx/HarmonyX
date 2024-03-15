e7fe27240d6c1a1ea876e5cca1fd1c2e18b6151a
kohanis <kohanis@ya.ru>
2024-03-16 00:58:21 +0300

    Implementing stack trace fixes tests

---
+++ HarmonyTests/Extras/TestStackTraceFixes.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using HarmonyLibTests;
+using NUnit.Framework;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace HarmonyTests.Extras;
+
+[TestFixture]
+class TestStackTraceFixes : TestLogger
+{
+	[Test]
+	public void Test_StackTraceFixes()
+	{
+		var harmony = new Harmony("test-stack-trace-fixes");
+		var getExecutingAssemblyTarget = SymbolExtensions.GetMethodInfo(() => GetExecutingAssemblyTarget());
+		var getMethodTarget = SymbolExtensions.GetMethodInfo(() => GetMethodTarget());
+		var dummyPrefix = SymbolExtensions.GetMethodInfo(() => DummyPrefix());
+
+		_ = harmony.Patch(getExecutingAssemblyTarget, new HarmonyMethod(dummyPrefix));
+		Assert.AreEqual(getExecutingAssemblyTarget.Module.Assembly, GetExecutingAssemblyTarget());
+
+		_ = harmony.Patch(getMethodTarget, new HarmonyMethod(dummyPrefix));
+		Assert.AreEqual(getMethodTarget, GetMethodTarget());
+	}
+
+
+	private static Assembly GetExecutingAssemblyTarget() =>
+		Assembly.GetExecutingAssembly();
+
+	private static MethodBase GetMethodTarget() =>
+		new StackTrace().GetFrame(0).GetMethod();
+
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	private static void DummyPrefix()
+	{
+	}
+}
