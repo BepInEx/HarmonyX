e34f1bdf98bb4aad4037575b847039efc3eaf02b
CptMoore <39010654+CptMoore@users.noreply.github.com>
2025-01-11 01:25:05 +0100

    Added unit test for extern instance method native detour patching.

--- HarmonyTests/Patching/Assets/NativeDetourClasses.cs
+++ HarmonyTests/Patching/Assets/NativeDetourClasses.cs
@@ -5,7 +5,54 @@ using System.Reflection.Emit;
 
 namespace HarmonyTests.Patching.Assets;
 
-public class ExternalMethod_Patch
+public class ExternalInstanceMethod_StringIsInterned_Patch
+{
+	public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) =>
+		instructions;
+
+	private const string UniqueString = nameof(ExternalInstanceMethod_StringIsInterned_Patch);
+
+	public const string PrefixInput = $"{UniqueString} {nameof(PrefixInput)}";
+	public const string PrefixOutput = $"{UniqueString} {nameof(PrefixOutput)}";
+	public static void Prefix(ref string __instance, ref string __result, ref bool __runOriginal)
+	{
+		if (__instance == PrefixInput)
+		{
+			__result = PrefixOutput;
+			__runOriginal = false;
+		}
+	}
+
+	public const string PostfixInput = $"{UniqueString} {nameof(PostfixInput)}";
+	public const string PostfixOutput = $"{UniqueString} {nameof(PostfixOutput)}";
+	public static void Postfix(ref string __instance, ref string __result)
+	{
+		if (__instance == PostfixInput)
+		{
+			__result = PostfixOutput;
+		}
+	}
+
+	public static readonly Type TranspiledException = typeof(UnauthorizedAccessException);
+	public static IEnumerable<CodeInstruction> TranspilerThrow(IEnumerable<CodeInstruction> instructions)
+	{
+		yield return new CodeInstruction(OpCodes.Newobj, TranspiledException.GetConstructor([]));
+		yield return new CodeInstruction(OpCodes.Throw);
+	}
+
+	public const string FinalizerInput = $"{UniqueString} {nameof(FinalizerInput)}";
+	public const string FinalizerOutput = $"{UniqueString} {nameof(FinalizerOutput)}";
+	public static Exception Finalizer(ref string __instance, ref string __result)
+	{
+		if (__instance == FinalizerInput)
+		{
+			__result = FinalizerOutput;
+		}
+		return null;
+	}
+}
+
+public class ExternalStaticMethod_MathCos_Patch
 {
 	public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) =>
 		instructions;
